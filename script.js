c0f26fa9e362a3120f32fc972d176171192bf453
birando <birger1@live.com>
2024-02-22 01:27:44 +0100

    feat: add Game over func, alert and retry option + comments
    Added Game over function and conditions to break out of game if player wins or loses + comments to describe what each function does.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+//Declaration of global variables
 let playerScore = 0;
 let computerScore = 0;
+let bestOfChoice = 0;
 
+//Declares game type of "best of 3", "best of 5" or "best of 10"
+function bestOfDeclaration() {
+  bestOfChoice = prompt(
+    "Choose game type\n\nBest of 3 - Type: '3'\n\nBest of 5 - Type: '5'\n\nBest of 10 - Type: '10'"
+  );
+  if (bestOfChoice === "3" || bestOfChoice === "5" || bestOfChoice === "10") {
+    bestOfChoice = parseInt(bestOfChoice);
+    return bestOfChoice + battle();
+  } else
+    return alert("Your input was invalid, try again") + bestOfDeclaration();
+}
+
+//Gets a weapon choice from computer based on random number generation
 function getComputerChoice() {
   let computerChoice;
   let weaponSelector = Math.floor(Math.random() * 100 + 1);
@@ -11,6 +26,7 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+//Gets a weapon choice from the player based on prompt input
 function getPlayerChoice() {
   let playerChoice = prompt("Choose by writing 'rock, 'paper' or 'scissors' ");
   playerChoice = playerChoice.toLocaleLowerCase();
@@ -23,6 +39,7 @@ function getPlayerChoice() {
   } else return alert("Your input was invalid, try again") + getPlayerChoice();
 }
 
+//Starts the battle between player and computer
 function battle() {
   let playerWeapon = getPlayerChoice();
   let computerWeapon = getComputerChoice();
@@ -41,7 +58,10 @@ function battle() {
         computerScore +
         "  Computer"
     );
-    battle();
+    if (bestOfChoice === playerScore) {
+      alert("CONGRATULATIONS! YOU WIN!");
+      gameOver();
+    } else battle();
   } else if (playerWeapon === computerWeapon) {
     alert(
       "Computer chose: " +
@@ -57,13 +77,29 @@ function battle() {
   alert(
     "Computer chose: " +
       computerWeapon +
-      "\n\nYOU LOOSE!\n\nYou " +
+      "\n\nYOU LOSE!\n\nYou " +
       playerScore +
       " - " +
       computerScore +
       "  Computer"
   );
-  battle();
+  if (bestOfChoice === computerScore) {
+    alert("YOU LOST!");
+    gameOver();
+  } else battle();
 }
 
-battle();
+//Game over mode after player or computer has won/lost and asks player to restart
+function gameOver() {
+  playerScore = 0;
+  computerScore = 0;
+  let playAgain = prompt(
+    "Thank you for playing!\n\nThis game is over, but you can try again by typing 'play'"
+  );
+  playAgain = playAgain.toLocaleLowerCase();
+  if (playAgain === "play") bestOfDeclaration();
+  else return alert("Your input was invalid, try again") + gameOver();
+}
+
+//Initializes the game
+bestOfDeclaration();
