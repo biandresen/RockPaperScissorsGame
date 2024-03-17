4c17f111691e3b19d43a0a8e198b7c31dfe881f0
birando <birger1@live.com>
2024-03-17 19:32:22 +0300

    feat: style buttons and add functions
    added functions: welcome message and buttons creation

--- script.js
+++ script.js
@@ -2,28 +2,35 @@ let divOtherContent = document.getElementById("otherContent");
 let divForMessage = document.createElement("div");
 let message1 = document.createElement("h1");
 let message2 = document.createElement("h3");
+let bestOf1Btn = document.createElement("button");
+let bestOf3Btn = document.createElement("button");
+let bestOf5Btn = document.createElement("button");
 
 window.onload = function () {
-  welcomeToGame();
+  addWelcomeMessage();
+  addBestOfButtons();
 };
 
-function welcomeToGame() {
+function addWelcomeMessage() {
   divOtherContent.appendChild(divForMessage);
   message1.textContent = "WELCOME";
   message2.textContent = "Choose to continue:";
   divForMessage.appendChild(message1);
   divForMessage.appendChild(message2);
+}
 
-  let bestof1Btn = document.createElement("button");
-  let bestof3Btn = document.createElement("button");
-  let bestof5Btn = document.createElement("button");
-  bestof1Btn.textContent = "Best of 1";
-  bestof3Btn.textContent = "Best of 3";
-  bestof5Btn.textContent = "Best of 5";
-  bestof1Btn.classList.add("defaultStyle");
-  bestof3Btn.classList.add("defaultStyle");
-  bestof5Btn.classList.add("defaultStyle");
-  divForMessage.appendChild(bestof1Btn);
-  divForMessage.appendChild(bestof3Btn);
-  divForMessage.appendChild(bestof5Btn);
+function addBestOfButtons() {
+  bestOf1Btn.textContent = "Best of 1";
+  bestOf3Btn.textContent = "Best of 3";
+  bestOf5Btn.textContent = "Best of 5";
+  bestOf1Btn.classList.add("defaultStyle");
+  bestOf3Btn.classList.add("defaultStyle");
+  bestOf5Btn.classList.add("defaultStyle");
+  divForMessage.appendChild(bestOf1Btn);
+  divForMessage.appendChild(bestOf3Btn);
+  divForMessage.appendChild(bestOf5Btn);
+
+  bestOf1Btn.addEventListener("click", playRound("rock"));
+  bestOf3Btn.addEventListener("click", playRound("paper"));
+  bestOf5Btn.addEventListener("click", playRound("scissors"));
 }
